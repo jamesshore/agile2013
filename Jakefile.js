6ce5447997d2fa951ce7756df89bfeae88673b46
James Shore <jshore@jamesshore.com>
2013-04-15 16:20:19 -0400

    Got JSHint working

--- Jakefile.js
+++ Jakefile.js
@@ -1,15 +1,46 @@
-// Copyright (c) 2013 Titanium I.T. LLC. All rights reserved. See LICENSE.TXT for details.
+// Copyright (c) 2012 Titanium I.T. LLC. All rights reserved. See LICENSE.txt for details.
+/*global desc, task, jake, fail, complete, directory, require, console, process */
 (function() {
 	"use strict";
 
-	desc("This is the default task");
-	task("default", ["example"], function() {
-		console.log("Hi, I'm the default");
+	var lint = require("./build/util/lint_runner.js");
+
+	desc("Lint and test");
+	task("default", ["lint"], function() {
+		console.log("\n\nOK");
 	});
 
-	desc("This is an example task");
-	task("example", function() {
-		console.log("Hello, world!");
+	desc("Lint everything");
+	task("lint", [], function() {
+		var passed = lint.validateFileList(filesToLint(), lintOptions(), {});
+		if (!passed) fail("Lint failed");
 	});
+
+	function filesToLint() {
+		var files = new jake.FileList();
+//		files.include("src/*.js");
+		files.include("Jakefile.js");
+		return files.toArray();
+	}
+
+	function lintOptions() {
+		return {
+			bitwise: true,
+			curly: false,
+			eqeqeq: true,
+			forin: true,
+			immed: true,
+			latedef: false,
+			newcap: true,
+			noarg: true,
+			noempty: true,
+			nonew: true,
+			regexp: true,
+			undef: true,
+			strict: true,
+			trailing: true,
+			browser: true
+		};
+	}
 
 }());
