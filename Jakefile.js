de16f565b98b934f4bd9e6a05335da45e157977d
James Shore <jshore@jamesshore.com>
2013-04-15 16:11:04 -0400

    Finished example Jakefile

--- Jakefile.js
+++ Jakefile.js
@@ -2,6 +2,11 @@
 (function() {
 	"use strict";
 
+	desc("This is the default task");
+	task("default", ["example"], function() {
+		console.log("Hi, I'm the default");
+	});
+
 	desc("This is an example task");
 	task("example", function() {
 		console.log("Hello, world!");
