bcd388a33e06cf0ad4a347037b600cad4dd93463
James Shore <jshore@jamesshore.com>
2013-04-15 16:09:24 -0400

    Added example task

---
+++ Jakefile.js
@@ -0,0 +1,10 @@
+// Copyright (c) 2013 Titanium I.T. LLC. All rights reserved. See LICENSE.TXT for details.
+(function() {
+	"use strict";
+
+	desc("This is an example task");
+	task("example", function() {
+		console.log("Hello, world!");
+	});
+
+}());
